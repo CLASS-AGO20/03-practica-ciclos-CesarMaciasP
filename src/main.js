947ff65f6ddd51979a3091204162f3f440ae8b66
CesarMaciasP <cmacias3@ucol.mx>
2020-12-09 16:30:25 -0600

    added obtenerDivisible

--- src/main.js
+++ src/main.js
@@ -1,3 +1,52 @@
-export default class App {}
+export default class App {
 
-let app = new App();
+    convertirAString(numero){ 
+        let string = 0
+        let resultado 
+
+        while(numero != 0) {
+            numero--
+            string++
+        } 
+
+        
+
+    }
+
+    obtenerDivisibles(numero) {
+        let i = numero
+        let divisibles = 0
+
+        do {
+            if (numero % i == 0) {
+                divisibles = divisibles + 1
+            }
+
+            i = i - 1
+
+        } while(i != 0)
+        
+        return divisibles
+    }
+
+    elevar(numero, potencia) {
+        let resultado = numero
+
+        while(potencia != 0) {
+            resultado = resultado * numero
+            potencia = potencia - 1
+        }
+
+        
+    }
+}
+
+let app = new App()
+
+console.log(app.factorial)
+
+console.log(app.convertirAString(4))
+
+console.log(app.obtenerDivisibles(6))
+
+console.log(app.elevar(3, 3))
